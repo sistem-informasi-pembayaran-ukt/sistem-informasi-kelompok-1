1e89064c2a78b6465b68d81c19ea9aa61b8e7f47
Nurul Utami Rusli <nurulutami.rusli@gmail.com>
2020-11-17 18:12:13 +0700

    maps

--- contact.php
+++ contact.php
@@ -15,6 +15,14 @@ session_start()
     <link rel="stylesheet" type="text/css" href="css/contact.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2F33n5dQ81WUE00s/" crossorigin="anonymous">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/css/mdb.min.css" rel="stylesheet">
 
     <title>Contact</title>
 </head>
@@ -23,19 +31,27 @@ session_start()
 <?php
 include "navbar.php";
 ?>
-<p id="judul"> Peta Kampus </p>
+<div class="container">
+<div id="maps"><h1>Peta Kampus</h1></div>
 <div class="box">
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15892.832806807173!2d119.49358618589373!3d-5.229958239582035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee72aa2cfa7b5%3A0x6866705455cbe08e!2sFAKULTAS%20TEKNIK%20UNHAS!5e0!3m2!1sid!2sid!4v1605549649721!5m2!1sid!2sid" width="750" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 </div>
 
-    
-    <p id="parag">
-        Kampus 1 <br> Kantor unit humas protokoler <br> Lantai 6 gedung rektorat kampus unhas tamalanrea <br> Jl. Perintis kemerdekaan km. 10 makassar, 90245 <br> Sulawesi selatan, INDONESIA <br> Telepon: +62 411 586200 | Fax: +62 411 585188 <br> e-mail:
-        ppid@unhas.ac.id
-    </p>
-    <p>
-        Kampus 2 <br> Jalan Poros Malino Km.6, Bontomarannu. <br> Gowa, Sulawesi Selatan. 92171 <br> e-mail : teknik@unhas.ac.id
-    </p>
+<div class="row">
+    <div class="col-sm-6 bg-transparent pt-5 pb-5">
+    Kampus 2 
+    <br> <i class="fas fa-map-marker-alt"></i> Jalan Poros Malino Km.6, Bontomarannu. </i> <br>  Gowa, Sulawesi Selatan. 92171 <br> <i class="far fa-envelope"> teknik@unhas.ac.id</i>
+    </div>
+    <div class="col-sm-6 bg-transparent pt-5 pb-5 text-center  d-flex align-item-center">
+    <a href="https://www.instagram.com/hasanuddin_univ/"><i class="fab fa-instagram mr-4"></i></a>
+    <a href="https://web.facebook.com/hasanuddin.university/?_rdc=1&_rdr"><i class="fab fa-facebook-f mr-4"></i></a>
+    <a href="https://unhas.ac.id/en"><i class="far fa-envelope mr-4"></i></a>
+    <a href="#"><i class="fab fa-twitter mr-4"></i></a>
+    </div>
+</div>
+</div>
+
+
     <div class="footer-copyright text-center py-3">
         <a>&copy; Copyright
             <a href="#">unhas.com</a>
@@ -44,4 +60,12 @@ include "navbar.php";
     </div>
 </body>
 
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/js/mdb.min.js"></script>
 </html>
