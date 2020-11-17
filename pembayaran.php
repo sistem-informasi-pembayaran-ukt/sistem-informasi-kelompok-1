c7df72d8f153cc9b1e2a9dafb5b41786a3f15789
Rofifah Nurul Annisa <rofifahnurul00@gmail.com>
2020-11-17 19:46:19 +0800

    pembayaran

--- pembayaran.php
+++ pembayaran.php
@@ -19,52 +19,52 @@ require_once "proses-pembayaran.php";
         //Tampilan maaf halaman ini tidak bisa diakses
         echo "<h3>Maaf Halaman Ini Tidak Bisa Diakses";
     }
-    else {
-
-    
+    else {   
 ?>
-<h3>Data Mahasiswa </h3>
+        <h3>Data Mahasiswa </h3>
 <?php
-    if (isset($_POST['detail']) ){
-        if ( isset($_SESSION['nim']) ) {
+        if (isset($_POST['detail']) ){
+            if ( isset($_SESSION['nim']) ) {
           
 ?>
-        <p>Nama : <?= dataMahasiswa('nama',$pdo);?></p>
-        <p>NIM :<?= dataMahasiswa('nim',$pdo);?></p>
-        <p>Golongan UKT :<?= dataMahasiswa('golonganUKT',$pdo);?><br>
-        <p>semester :<?= dataMahasiswa('namaSemester',$pdo);?><br>
-        <p>jurusan :<?= dataMahasiswa('namaJurusan',$pdo);?><br>
-        <p>tarif ukt :<?=dataMahasiswa('tarifUKT',$pdo);?><br>
-        <form method="post">
-            <input type="submit" name="sembunyikanDetail"  value="Sembunyikan Detail" /> 
-        </form>
+                <p>Nama : <?= dataMahasiswa('nama',$pdo);?></p>
+                <p>NIM :<?= dataMahasiswa('nim',$pdo);?></p>
+                <p>Golongan UKT :<?= dataMahasiswa('golonganUKT',$pdo);?><br>
+                <p>semester :<?= dataMahasiswa('namaSemester',$pdo);?><br>
+                <p>jurusan :<?= dataMahasiswa('namaJurusan',$pdo);?><br>
+                <p>tarif ukt :<?=dataMahasiswa('tarifUKT',$pdo);?><br>
+                <form method="post">
+                    <input type="submit" name="sembunyikanDetail"  value="Sembunyikan Detail" /> 
+                </form>
+<?
+            }
+        }
+
+        else if (!isset($_POST['detail']) || isset($_POST['sembunyikanDetail'])){
+?> 
+            <form method="post">
+                <input type="submit" name="detail"  value="Tampilkan Detail" /> 
+            </form>
 <?
         }
-    }
-    else if (!isset($_POST['detail']) || isset($_POST['sembunyikanDetail'])){
-    ?> <form method="post">
-    <input type="submit" name="detail"  value="Tampilkan Detail" /> 
-</form><?
-    }
     
 ?>
-<h3>Jika data anda sudah sesuai, silahkan lanjutkan untuk pelaporan pembayaran</h3>
+        <h3>Jika data anda sudah sesuai, silahkan lanjutkan untuk pelaporan pembayaran</h3>
     
-<form method="post" class="inner-login" action="proses-pembayaran.php" enctype="multipart/form-data">
+        <form method="post" class="inner-login" action="proses-pembayaran.php" enctype="multipart/form-data">
        
                 
-        <h5>Tambahkan bukti pembayaran dalam bentuk gambar ataupun pdf, maksimal ukuran 1 MB</h5>
-        <div class="form-group">
-            <input type="file" name="bukti"accept="*/image"></p>
-        </div>
-        <input type="hidden" value="<?php echo date("Y-m-d");?>" name="tanggal"/></p>
-        <input type="submit" value="Submit" name="submit"/></p>
-</form>
+            <h5>Tambahkan bukti pembayaran dalam bentuk gambar ataupun pdf, maksimal ukuran 1 MB</h5>
+            <div class="form-group">
+                <input type="file" name="bukti"accept="*/image">
+            </div>
+            <input type="hidden" value="<?php echo date("Y-m-d");?>" name="tanggal">
+            <input type="submit" value="Submit" name="submit">
+        </form>
 
 <?
     }
-  ?>      
+?>      
 
 </body>
-
 </html>
